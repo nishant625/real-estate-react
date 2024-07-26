e21aa39a1666c8d5e26cf1511c8a8583abaf0329
nishant625 <gangwarnishant625@gmail.com>
2024-07-26 14:41:32 +0530

    delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 function Profile() {
@@ -85,6 +88,28 @@ function Profile() {
     }
   };
 
+  const handleDeleteUser=async()=>{
+    try {
+      dispatch(deleteUserStart());
+      const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      })
+        const data=await res.json();
+        if(data.success===false){
+          dispatch(deleteUserFailure(data.message));
+          return;
+        }
+        dispatch(deleteUserSuccess(data))
+      
+
+
+     } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+      
+    }
+  }
+
+
 
   return (
     <div className=" p-3 max-w-lg mx-auto">
@@ -141,7 +166,7 @@ function Profile() {
       </form>
 
       <div className=" flex justify-between mt-5">
-        <span className=" text-red-700 cursor-pointer">Delete account</span>
+        <span onClick={handleDeleteUser} className=" text-red-700 cursor-pointer">Delete account</span>
         <span className=" text-red-700 cursor-pointer">Sign out</span>
       </div>
 
