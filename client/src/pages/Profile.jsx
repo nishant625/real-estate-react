ab37d0329bbe03d89bcfe12f106fd8c8f51397d3
nishant625 <gangwarnishant625@gmail.com>
2024-07-27 14:04:40 +0530

    complete update lisitng page func

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -279,7 +279,9 @@ function Profile() {
                 >
                   Delete
                 </button>
+                <Link to={`/update-listing/${listing._id}`}>
                 <button className=" text-green-500 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
