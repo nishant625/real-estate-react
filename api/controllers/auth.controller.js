73f346dd513e058b8d36a27ab649fec36dd8b4c6
Manas Singhal <62430941+manassinghal95@users.noreply.github.com>
2024-07-28 03:01:18 +0530

    Update signup

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,17 @@ import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
-
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("user created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
